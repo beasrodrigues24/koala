6c7e5240a7e5ab69d7985b641cecd6c9f38192f9
zer0-5 <guilherme2001fernandes@hotmail.com>
2022-05-09 15:02:59 +0100

    Use classes for lexer and parser

--- src/lexer.py
+++ src/lexer.py
@@ -1,46 +1,49 @@
-from ply import lex 
+from ply import lex
 
-tokens = ['VAR', 'WORD', 'NEWLINE', 'TMPVAR', 'IF', 'ELIF', 'ELSE', 'FOR']
-literals = ['{', '}', ':']
+class Lexer:
 
-t_ignore = ' \t'
+    def __init__(self):
+        self.lexer = lex.lex(module=self, debug=False)
 
-def t_VAR(t):
-    r'\$[A-Za-z_]\w*(\.\w+)*'
-    t.value = t.value[1:].split('.')
-    return t
+    tokens = ['VAR', 'WORD', 'NEWLINE', 'TMPVAR', 'IF', 'ELIF', 'ELSE', 'FOR']
+    literals = ['{', '}', ':']
 
-def t_TMPVAR(t):
-    r'\#[A-Za-z_]\w*'
-    return t
+    t_ignore = ' \t'
 
-def t_WORD(t):
-    r'\w+'
-    return t
+    def t_VAR(self, t):
+        r'\$[A-Za-z_]\w*(\.\w+)*'
+        t.value = t.value[1:].split('.')
+        return t
 
-def t_NEWLINE(t):
-    r'\n'
-    t.lexer.lineno += 1
-    return t
+    def t_TMPVAR(self, t):
+        r'\#[A-Za-z_]\w*'
+        return t
 
-def t_IF(t):
-    r'@if'
-    return t
+    def t_WORD(self, t):
+        r'\w+'
+        return t
 
-def t_ELIF(t):
-    r'@elif'
-    return t
+    def t_NEWLINE(self, t):
+        r'\n'
+        t.lexer.lineno += 1
+        return t
 
-def t_ELSE(t):
-    r'@else'
-    return t 
+    def t_IF(self, t):
+        r'@if'
+        return t
 
-def t_FOR(t):
-    r'@for'
-    return t
+    def t_ELIF(self, t):
+        r'@elif'
+        return t
 
-def t_error(t):
-    print('ERROR', t)
-    t.lexer.skip(1)
+    def t_ELSE(self, t):
+        r'@else'
+        return t
 
-lexer = lex.lex()
+    def t_FOR(self, t):
+        r'@for'
+        return t
+
+    def t_error(self, t):
+        print('ERROR', t)
+        t.lexer.skip(1)
--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,5 @@
-from ply import yacc 
-from lexer import tokens
+from ply import yacc
+from lexer import Lexer
 import sys
 
 dic = {
@@ -11,111 +11,122 @@ dic = {
     }
 }
 
-def p_lang(p):
-    'lang : statements'
-    p[0] = p[1]
+class Parser:
+    def __init__(self, dic):
+        self.dic = dic
+        self.lexer = Lexer()
+        self.tokens = self.lexer.tokens
+        self.parser = yacc.yacc(module=self, write_tables=1)
 
-def p_statements(p):
-    'statements : statements statement'
-    p[0] = p[1] + p[2]
+    def parse(self, data):
+        self.parser.parse(data)
 
-def p_statements_empty(p):
-    'statements : '
-    p[0] = ""
-
-def p_statement_conditionals(p):
-    'statement : if elifs else'
-    if p[1]:
+    def p_lang(self, p):
+        'lang : statements'
         p[0] = p[1]
-    elif p[2]:
-        p[0] = p[2]
-    elif p[3]:
-        p[0] = p[3]
-    else: 
-        p[0] = ""
-    
-def p_if(p):
-    'if : IF VAR "{" statements "}"'
-    if p[2] in dic: 
-        p[0] = p[4]
-    else:
+
+    def p_statements(self, p):
+        'statements : statements statement'
+        p[0] = p[1] + p[2]
+
+    def p_statements_empty(self, p):
+        'statements : '
         p[0] = ""
 
-def p_elifs(p):
-    'elifs : elifs elif'
-    if p[1]:
+    def p_statement_conditionals(self, p):
+        'statement : if elifs else'
+        if p[1]:
+            p[0] = p[1]
+        elif p[2]:
+            p[0] = p[2]
+        elif p[3]:
+            p[0] = p[3]
+        else:
+            p[0] = ""
+
+    def p_if(self, p):
+        'if : IF VAR block'
+        if p[2] in dic:
+            p[0] = p[3]
+        else:
+            p[0] = ""
+
+    def p_elifs(self, p):
+        'elifs : elifs elif'
+        if p[1]:
+            p[0] = p[1]
+        elif p[2]:
+            p[0] = p[2]
+        else:
+            p[0] = ""
+
+    def p_elif(self, p):
+        'elif : ELIF VAR block'
+        if p[2] in self.dic:
+            p[0] = p[3]
+        else:
+            p[0] = ""
+
+    def p_elifs_empty(self, p):
+        'elifs : '
+        p[0] = ""
+
+    def p_else(self, p):
+        'else : ELSE block'
+        p[0] = p[2]
+
+    def p_else_empty(self, p):
+        'else : '
+        p[0] = ""
+
+    def p_statement_for(self, p):
+        'statement : FOR TMPVAR ":" var block'
+        p[0] = ""
+        for x in p[4]:
+            p[0] += p[5].strip().replace(p[2], x) + '\n'
+
+    def p_block(self, p):
+        'block : "{" statements "}"'
         p[0] = p[1]
-    elif p[2]:
-        p[0] = p[2]
-    else:
-        p[0] = ""
 
-def p_elifs_empty(p):
-    'elifs : '
-    p[0] = ""
+    def p_statement_phrase(self, p):
+        'statement : phrase NEWLINE'
+        p[0] = p[1] + '\n'
 
-def p_elif(p):
-    'elif : ELIF VAR "{" statements "}"'
-    if p[2] in dic: 
-        p[0] = p[4]
-    else:
-        p[0] = ""
+    def p_phrase(self, p):
+        'phrase : phrase word'
+        spaces = ''
+        if p[1]:
+            spaces = ' '
+        p[0] = p[1] + spaces + p[2]
 
-def p_else(p):
-    'else : ELSE "{" statements "}"'
-    p[0] = p[3]
+    def p_phrase_empty(self, p):
+        'phrase : '
+        p[0] = ''
 
-def p_else_empty(p):
-    'else : '
-    p[0] = ""
+    def p_word(self, p):
+        '''
+        word : WORD
+            | var
+            | TMPVAR
+        '''
+        p[0] = p[1]
 
-def p_statement_phrase(p):
-    'statement : phrase NEWLINE'
-    p[0] = p[1] + '\n'
+    def p_var(self, p):
+        'var : VAR'
+        tmp = dic
+        for x in p[1]:
+            tmp = tmp[x]
+        p[0] = tmp
 
-def p_phrase(p): 
-    'phrase : phrase word'
-    spaces = ''
-    if p[1]:
-        spaces = ' '
-    p[0] = p[1] + spaces + p[2] 
-
-def p_phrase_empty(p):
-    'phrase : '
-    p[0] = ''
-
-def p_word(p):
-    '''
-    word : WORD
-         | var
-         | TMPVAR
-    '''
-    p[0] = p[1]
-
-def p_var(p):
-    'var : VAR'
-    tmp = dic
-    for x in p[1]:
-        tmp = tmp[x]
-    p[0] = tmp
-    
-def p_statement_for(p):
-    'statement : FOR TMPVAR ":" var "{" statements "}"'
-    p[0] = ""
-    for x in p[4]: 
-        p[0] += p[6].strip().replace(p[2], x) + '\n'
-
-def p_error(p):
-    print("Syntax error: ", p)
+    def p_error(self, p):
+        print("Syntax error: ", p)
 
 file = open(sys.argv[1], "r")
 content = file.read()
 
-parser = yacc.yacc()
+parser = Parser(dic)
 
 result = parser.parse(content)
 
 print(result)
-
-#output = open("output.txt", "w")
-#output.write(result)
